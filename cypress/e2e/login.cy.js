ea18875589c8c92f4e385497ba58c036d6be0d0c
Mateus Queiroz <dentistaqfl@gmail.com>
2025-04-14 19:18:16 -0300

    First commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,17 @@
+describe('Orange HMN Tests', () => {
+  it('Login com sucesso', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+  })
+})
+
+it('Login Falho', () => {
+  cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Test')
+  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Test')
+  cy.get('.oxd-button').click()
+  cy.get('.oxd-alert-content')
+})

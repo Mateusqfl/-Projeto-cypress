3a9075576b243dccda3d865b63e517bba1337cb2
Mateus Queiroz <dentistaqfl@gmail.com>
2025-04-15 14:32:00 -0300

    Adding user data json

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,17 +1,24 @@
+import userData from '../fixtures/userData.json'
+
 describe('Orange HMN Tests', () => {
+
+
   it('Login com sucesso', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(userData.userSucess.UserName)
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(userData.userSucess.password)
     cy.get('.oxd-button').click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
   })
+
+  it('Login Falho', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(userData.userFail.UserName)
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(userData.userFail.password)
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert-content')
+  })
+  
 })
 
-it('Login Falho', () => {
-  cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Test')
-  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Test')
-  cy.get('.oxd-button').click()
-  cy.get('.oxd-alert-content')
-})
+

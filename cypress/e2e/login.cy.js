d24918c4ebc6ae4eabcf5a4d3239ac6007f68c4e
Mateus Queiroz <dentistaqfl@gmail.com>
2025-04-17 15:08:29 -0300

    added radon

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,27 @@
+import userData from '../fixtures/userData.json'
+
+
+describe('Orange HMN Tests', () => {
+
+
+    it('user - Sucess', () => {
+        cy.visit('/auth/login')
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(userData.userSucess.UserName)
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(userData.userSucess.password)
+        cy.get('.oxd-button').click()
+        cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+        
+    })
+    
+    
+    
+    it('Login Falho', () => {
+        cy.visit('/auth/login')
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(userData.userFail.UserName)
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(userData.userFail.password)
+        cy.get('.oxd-button').click()
+        cy.get('.oxd-alert-content')
+      })
+
+
+})
